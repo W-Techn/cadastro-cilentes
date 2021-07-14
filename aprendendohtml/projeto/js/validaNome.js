7b65c4fefda4a57452f159aeb9f6ad18f952ae93
kaduzitoh <77650431+kaduzitoh@users.noreply.github.com>
2021-07-14 13:12:12 -0300

    Correção de bug

--- aprendendohtml/projeto/js/validaNome.js
+++ aprendendohtml/projeto/js/validaNome.js
@@ -3,11 +3,11 @@ var msg2 = document.querySelector('#too-short');
 
 inputNumero.addEventListener("focus", function () {
     if (inputName.value.length < 12) {
-        inputNumero.style.borderColor = "darkred";
+        inputName.style.borderColor = "darkred";
         msg2.textContent = "O nome não pode ser tão curto";
         button.classList.add('disabled');
     } else {
-        inputNumero.style.borderColor = "rgba(238,156,167,.25)";
+        inputName.style.borderColor = "rgba(238,156,167,.25)";
         msg2.textContent = "";
         button.classList.remove('disabled');
     }
